b1e3eb1a38c886416369558b316425aa76b73e2a
houxiufeng <houxiufeng@didichuxing.com>
2018-08-08 10:35:08 +0800

    add api test

--- src/main/java/com/example/apitest/TestApiController.java
+++ src/main/java/com/example/apitest/TestApiController.java
@@ -1,5 +1,6 @@
 package com.example.apitest;
 
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +10,14 @@ public class TestApiController {
     public String test1(String name) {
         return "test api get value: " + name;
     }
+
+    @RequestMapping("/test2")
+    public JSONObject test2() {
+        JSONObject json = new JSONObject();
+        json.put("name", "fitz");
+        json.put("age", 30);
+        json.put("gender", true);
+        return json;
+    }
+
 }
