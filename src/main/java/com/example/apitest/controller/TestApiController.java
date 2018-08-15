541e48c3aebf677792b789f0d2e9227abcedc897
houxiufeng <houxiufeng@didichuxing.com>
2018-08-15 14:15:33 +0800

    fix bug

--- src/main/java/com/example/apitest/controller/TestApiController.java
+++ src/main/java/com/example/apitest/controller/TestApiController.java
@@ -1,4 +1,4 @@
-package com.example.apitest;
+package com.example.apitest.controller;
 
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,14 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestApiController {
     @RequestMapping("/test1")
     public String test1(String name) {
-        return "test api get value: " + name;
+        return "test api get value is: " + name;
     }
 
     @RequestMapping("/test2")
     public JSONObject test2() {
         JSONObject json = new JSONObject();
         json.put("name", "fitz");
-        json.put("age", 30);
+        json.put("age", 33);
         json.put("gender", true);
         return json;
     }
